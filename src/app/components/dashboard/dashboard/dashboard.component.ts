e9232b1eca04600229e7aa6e722e9dfcee161a8a
Austin <victor.d@swipe.ng>
2025-02-25 10:31:20 +0100

    card section on dashboard module

--- src/app/components/dashboard/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard/dashboard.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 
 import { Router, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { materials } from '../../../shared/angular-material/material.module';
+import { SharedModule } from '../../../shared/shared.module';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, materials],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { materials } from './angular-material/material.module';
-
+// import { ChartsModule } from 'ng2-charts';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    ...materials
+    ...materials,
+    
+    
   ]
 })
 export class SharedModule { }
